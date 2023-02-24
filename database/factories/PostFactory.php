1304b4b8b0386d9030d9a2f9aec2444106077d20
Пользователь <fireworld@i.ua>
2023-02-24 22:09:47 +0200

    lesson 1

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Post;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    protected $model = Post::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->name,
+            'content' => $this->faker->text,
+            'image' => $this->faker->word() . '.jpg',
+            'likes' => $this->faker->numberBetween(0, 200),
+        ];
+    }
+}
